63579d28b1d687da23f3d182ae85bd859d9f4e96
jheater1 <jheater1@asu.edu>
2025-03-10 10:56:27 -0700

    Add Get all chores feature

---
+++ ChoreChamp.API/Features/Chores/GetAllChores/GetAllChoresEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using ChoreChamp.API.Infrastructure.Persistence;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChoreChamp.API.Features.Chores.GetAllChores;
+
+public class GetAllChoresEndpoint(ChoreChampDbContext dbContext) : 
+    Ep.NoReq.Res<IEnumerable<GetAllChoresResponse>>.Map<GetAllChoresMapper>
+{
+    public override void Configure()
+    {
+        Get("api/chores");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        var chores = await dbContext.Chores.ToListAsync(c);
+        await SendAsync(Map.FromEntity(chores));
+    }
+}
---
+++ ChoreChamp.API/Features/Chores/GetAllChores/GetAllChoresResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace ChoreChamp.API.Features.Chores.GetAllChores;
+
+public record GetAllChoresResponse(int Id, string Name, string Description, int Points);
+
