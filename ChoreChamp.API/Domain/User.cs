a8aa1955c33b2d3f5aa8723fb10a47f654e0ca41
jheater1 <jheater1@asu.edu>
2025-03-10 13:28:25 -0700

    Create User domain class

---
+++ ChoreChamp.API/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace ChoreChamp.API.Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public bool IsParent { get; set; }
+    public int Points { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
