b99c5576b2d70536ab41d2bff4046900de4e0a09
jheater1 <jheater1@asu.edu>
2025-03-10 10:57:08 -0700

    Update endpoint to use fluent req and response inheritence

--- ChoreChamp.API/Features/Chores/CreateChore/CreateChoreEndpoint.cs
+++ ChoreChamp.API/Features/Chores/CreateChore/CreateChoreEndpoint.cs
@@ -4,14 +4,9 @@ using FastEndpoints;
 
 namespace ChoreChamp.API.Features.Chores.CreateChore;
 
-public class CreateChoreEndpoint : Endpoint<CreateChoreRequest, CreateChoreResponse, CreateChoreMapper>
+public class CreateChoreEndpoint(ChoreChampDbContext dbContext) : 
+    Ep.Req<CreateChoreRequest>.Res<CreateChoreResponse>.Map<CreateChoreMapper> 
 {
-    private readonly ChoreChampDbContext _dbContext;
-    public CreateChoreEndpoint(ChoreChampDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
     public override void Configure()
     {
         Post("api/chores");
@@ -21,8 +16,8 @@ public class CreateChoreEndpoint : Endpoint<CreateChoreRequest, CreateChoreRespo
     public override async Task HandleAsync(CreateChoreRequest r, CancellationToken c)
     {
         Chore chore = Map.ToEntity(r);
-        _dbContext.Chores.Add(chore);
-        await _dbContext.SaveChangesAsync(c);
+        dbContext.Chores.Add(chore);
+        await dbContext.SaveChangesAsync(c);
         Response = Map.FromEntity(chore);
         await SendCreatedAtAsync<CreateChoreEndpoint>(new {id = chore.Id}, Response);
     }
