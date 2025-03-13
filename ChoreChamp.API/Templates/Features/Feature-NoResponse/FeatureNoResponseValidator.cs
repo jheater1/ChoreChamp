641f1c8ad703c694223fb05871bdaa9b82310850
Daniel Wathen <daniel.wathen@microchip.com>
2025-03-13 14:33:21 -0700

    Created the three feature templates and powershell script to install templates locally

---
+++ ChoreChamp.API/Templates/Features/Feature-NoResponse/FeatureNoResponseValidator.cs
@@ -0,0 +1,12 @@
+﻿using ChoreChamp.API.Templates.Features.Feature;
+using FastEndpoints;
+
+namespace ChoreChamp.API.Templates.Features.Feature_NoResponse;
+
+public class FeatureNoResponseValidator : Validator<FeatureNoResponseRequest>
+{
+    public FeatureNoResponseValidator()
+    {
+        
+    }
+}
